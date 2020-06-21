b63bd5b3dd57486babd3ddbcf8c5bbaff3717cf2
Evgenii Dolgov <river100@mail.ru>
2020-06-21 23:32:27 +0300

    Версия проекта повышена до 0.9

--- src/main/java/amaralus/apps/rogue/MainApplication.java
+++ src/main/java/amaralus/apps/rogue/MainApplication.java
@@ -28,7 +28,7 @@ public class MainApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Rogue v-0.8");
+        primaryStage.setTitle("Rogue v-0.9");
         primaryStage.setResizable(false);
 
         primaryStage.setScene(scene);
